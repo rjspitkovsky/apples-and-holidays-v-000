a74bbcaccd8808e9bf33578668c4af3676f9b2cb
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-26 00:33:10 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
   puts season.to_s.capitalize! + ":"
     holiday.each do |item|
-      puts item.capitalize
+      puts item
     end
   end
 end
