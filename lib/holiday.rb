231c45c57701a5faadeaf554f9f5f2184fe52a69
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-26 00:11:23 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,7 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-holiday_hash[:spring][:memorial_day].join
+holiday_hash[:summer][:fourth_of_july][2]
 
 end
 
